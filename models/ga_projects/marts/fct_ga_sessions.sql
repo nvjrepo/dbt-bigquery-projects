8274940dbbcbb753f908b26d8f64fb759253cd9e
nam-97 <nam.nx@joonsolutions.com>
2024-02-28 15:22:08 +0700

    fix

--- models/ga_projects/marts/fct_ga_sessions.sql
+++ models/ga_projects/marts/fct_ga_sessions.sql
@@ -1,9 +1,27 @@
+{{
+    config(
+        unique_key='unique_id',
+        incremental_strategy='merge', 
+        partition_by={
+            "field":"session_started_at",
+            "data_type": "timestamp",
+            "granularity": "day"            
+        }
+
+    )
+}}
+
+{% set date_grains = ['day','week','month'] %}
+
 select
     unique_id,
     full_visitor_id,
     session_started_at,
     visit_number,
     channel_grouping,
-    totals.bounce is not null then is_bounce,
+    totals.bounces is not null as is_bounced,
+    {% for date_grain in date_grains -%}
+        date_trunc(session_started_at, {{ date_grain }}) as session_started_{{ date_grain }}{{ ',' if not loop.last }}
+    {%- endfor %}
 
 from {{ ref('base_ga__sessions') }}
